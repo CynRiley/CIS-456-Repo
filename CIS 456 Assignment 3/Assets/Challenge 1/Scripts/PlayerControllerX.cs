dce1ff89a507c9438fd0b89201a5fcbd8e9acecb
Robyn Riley <rriley818@gmail.com>
2019-09-11 15:26:20 -0500

    Initial Project

---
+++ CIS 456 Assignment 3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Robyn Riley
+* PlayerControllerX.cs
+* Assignment 3
+* This script is what keeps the plane moving.
+*/
+
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.back * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+    }
+}
