e5ab0c6762c52c07f7242e1e723321a83e6f65bd
Robyn Riley <rriley818@gmail.com>
2019-11-08 22:36:18 -0600

    Assignment 9 Push

---
+++ Assignment 9/Assets/Assignment 9 Assets/Open.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Robyn Riley
+ * Open.cs
+ * Assignment 9
+ * Applies a force to the door when the player is detected
+ */
+
+
+public class Open : MonoBehaviour
+{
+    public float length;
+    public float waitTime;
+    public float openForce;
+
+    bool open, pause;
+
+    public LayerMask player;
+
+    RaycastHit ray;
+
+    private void FixedUpdate()
+    {
+        if (!pause)
+        {
+            Physics.Raycast(transform.position, transform.forward * length, out ray, length, player);
+            Debug.DrawRay(transform.position, transform.forward * length, Color.magenta);
+        }
+        if (ray.collider != null)
+        {
+            Invoke("Swing", 0);
+        }
+
+    }
+
+    private void Swing()
+    {
+        pause = true;
+
+        GetComponent<Rigidbody>().AddForce(-transform.forward * openForce, ForceMode.Force);
+
+        Invoke("Lock", .5f);
+
+        Invoke("Close", waitTime);
+
+    }
+
+    private void Lock()
+    {
+
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    private void Close()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        Invoke("Set", 1);
+    }
+
+    private void Set()
+    {
+        pause = false;
+    }
+}
---
+++ Assignment 9/Assets/Assignment 9 Assets/Push.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Robyn Riley
+ * Push.cs
+ * Assignment 9
+ * Launches the player when they pass over the trigger.
+ */
+
+public class Push : MonoBehaviour
+{
+    public ForceMode force;
+    public float amount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(force == ForceMode.Impulse || force == ForceMode.VelocityChange)
+        {
+            Debug.Log(force);
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * amount);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(force == ForceMode.Acceleration || force == ForceMode.Force)
+        {
+            Debug.Log(force);
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * amount);
+        }
+    }
+}
