a0896857e19fb6b3957acc2b0862482168f8e06c
Robyn Riley <rriley818@gmail.com>
2019-11-22 13:21:12 -0600

    Assignment 11 Push

---
+++ Assignment 11/Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+/*
+ * Robyn Riley
+ * PlayerController.cs
+ * Assignment 11
+ * Moves agent based on click position
+ */
+
+
+public class PlayerController : MonoBehaviour
+{ 
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        if (character != null)
+        {
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+    }
+}
